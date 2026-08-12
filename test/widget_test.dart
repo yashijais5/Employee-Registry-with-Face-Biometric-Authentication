18dca1fb89f5e1b83f1650842e48cf0f09b9fc9b
yashaswi jaiswal <167241629+yashijais5@users.noreply.github.com>
2026-08-12 11:08:34 +0530

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,7 +1,20 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+
 import '../lib/main.dart';
+import '../lib/services/database_service.dart';
 
 void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  setUpAll(() async {
+    await DatabaseService.init();
+  });
+
+  tearDownAll(() async {
+    await Hive.close();
+  });
+
   testWidgets('MyApp launches successfully', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
